f0c3e3630a31279adf88261d0fe1f7dd81ee1b87
evan2916 <644568491@qq.com>
2015-02-19 22:25:18 +0800

    Edmond initial canvas.

--- routes/html5S.js
+++ routes/html5S.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* HTML5 Study. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.render('html5S', { title: 'ED JJ HOME' });
 });
 
 module.exports = router;
