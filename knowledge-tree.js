1c2328323e458f8e6028ded424ab29ea3c141dfb
EdmondWang <644568491@qq.com>
2018-07-03 23:04:50 +0800

    Enrich Knowledge

--- knowledge-tree.js
+++ knowledge-tree.js
@@ -1,260 +1,429 @@
-(function() {
+(function () {
   "use strict";
-  var treeData =
-  {
+  var treeData = {
     "name": "Root",
-    "children": [
-      {
+    "children": [{
         "name": "Database",
         "children": [{
-          "name": "RDBMS"
-        },{
+          "name": "RDBMS",
+          "children": [{
+            "name": "Oracle",
+            "children": [{
+              "name": "Join"
+            }]
+          }]
+        }, {
           "name": "No-SQL"
         }]
       },
       {
         "name": "Server",
         "children": [{
-          "name": "Java"
+          "name": "Java EE",
+          "children": [{
+            "name": "Servlet"
+          }, {
+            "name": "Filter"
+          }, {
+            "name": "Listener"
+          }, {
+            "name": "JDBC"
+          }, {
+            "name": "Spring Framework",
+            "children": [{
+              "name": "Controller"
+            }, {
+              "name": "DI"
+            }, {
+              "name": "AOP"
+            }]
+          }]
         }]
       },
-      { "name": "WEB",
-      "children": [
-        { 
-          "name": "HTML",
-          "children": [
-            { "name": "Text & Image" },
-            { "name": "Hyper Link" },
-            { "name": "Form" },
-            { "name": "Table" },
-            { "name": "Shadow Root" }
-          ]
-        },
-        { "name": "CSS Stylesheet",
-          "children": [
-            { "name": "CSS BOX" },
-            { "name": "Layout" },
-            { "name": "Position" },
-            { "name": "Transfrom" },
-            { "name": "Transition" },
-            { "name": "Animation" },
-            { "name": "Responsive Design" },
-          ]
-        },
-        { "name": "JavaScript",
-          "children": [
-            { "name": "String" },
-            { "name": "Object" },
-            { "name": "Array" },
-            { "name": "Function" },
-            { "name": "Number" },
-            { "name": "Boolean" },
-            { "name": "Undefined & Null" },
-            { "name": "Scope Chain" },
-            { "name": "Event" },
-          ]
-        },
-        { "name": "HTTP Protocol",
-          "children": [
-            { "name": "GET & POST" },
-            { "name": "AJAX" },
-            { "name": "Cache" },
-            { "name": "HTTPS" },
-            { "name": "CORS",
-              "children": [
-                { "name": "JSONP" },
-                { "name": "XMLHttpRequest" }
-              ]
-            },
-          ]
-        },
-        { "name": "SVG",
-          "children": [
-            { "name": "Draw Path" },
-            { "name": "Draw Arc" },
-            { "name": "Styling" }
-          ]
-        },
-        { "name": "Canvas"},
-        { "name": "Test"}
-      ]
-    }]
+      {
+        "name": "WEB",
+        "children": [{
+            "name": "HTML",
+            "children": [{
+                "name": "Text & Image"
+              },
+              {
+                "name": "Hyper Link"
+              },
+              {
+                "name": "Form",
+                "children": [{
+                  "name": "<input />"
+                }, {
+                  "name": "<select />"
+                }, {
+                  "name": "<textarea />"
+                }]
+              },
+              {
+                "name": "Table"
+              },
+              {
+                "name": "Shadow Root"
+              }
+            ]
+          },
+          {
+            "name": "CSS Stylesheet",
+            "children": [{
+                "name": "Color"
+              }, {
+                "name": "CSS BOX"
+              },
+              {
+                "name": "Layout"
+              },
+              {
+                "name": "Position",
+                "children": [{
+                  "name": "Fixed",
+                }, {
+                  "name": "Relative"
+                }, {
+                  "name": "Absolute"
+                }, {
+                  "name": "Sticky"
+                }]
+              },
+              {
+                "name": "Pseudo-Element"
+              },
+              {
+                "name": "Transfrom"
+              },
+              {
+                "name": "Transition"
+              },
+              {
+                "name": "Animation"
+              },
+              {
+                "name": "Responsive Design",
+                "children": [{
+                  "name": "Media Query"
+                }]
+              },
+            ]
+          },
+          {
+            "name": "JavaScript",
+            "children": [{
+                "name": "String",
+                "children": [{
+                  "name": "substr"
+                }, {
+                  "name": "substring"
+                }]
+              },
+              {
+                "name": "Object"
+              },
+              {
+                "name": "Array",
+                "children": [{
+                  "name": "forEach"
+                }, {
+                  "name": "push"
+                }, {
+                  "name": "pop"
+                }]
+              },
+              {
+                "name": "Function"
+              },
+              {
+                "name": "Number"
+              },
+              {
+                "name": "Boolean"
+              },
+              {
+                "name": "Undefined & Null"
+              },
+              {
+                "name": "Scope Chain"
+              },
+              {
+                "name": "Event"
+              },
+              {
+                "name": "DOM",
+                "children": [{
+                  "name": "Append"
+                }, {
+                  "name": "InsertAfter"
+                }]
+              },
+              {
+                "name": "Third Party Library",
+                "children": [{
+                  "name": "jQuery"
+                }, {
+                  "name": "MV* framework",
+                  "children": [{
+                    "name": "Angular JS"
+                  }, {
+                    "name": "React JS"
+                  }, {
+                    "name": "Vue JS"
+                  }]
+                }]
+              }
+
+            ]
+          },
+          {
+            "name": "HTTP Protocol",
+            "children": [{
+                "name": "GET & POST"
+              },
+              {
+                "name": "AJAX"
+              },
+              {
+                "name": "Cache"
+              },
+              {
+                "name": "HTTPS"
+              },
+              {
+                "name": "CORS",
+                "children": [{
+                    "name": "JSONP"
+                  },
+                  {
+                    "name": "XMLHttpRequest"
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            "name": "SVG",
+            "children": [{
+                "name": "Draw Path"
+              },
+              {
+                "name": "Draw Arc"
+              },
+              {
+                "name": "Styling"
+              }
+            ]
+          },
+          {
+            "name": "Canvas"
+          },
+          {
+            "name": "Test"
+          }
+        ]
+      }
+    ]
   };
 
-// Set the dimensions and margins of the diagram
-var margin = {top: 20, right: 90, bottom: 30, left: 90},
+  // Set the dimensions and margins of the diagram
+  var margin = {
+      top: 20,
+      right: 90,
+      bottom: 30,
+      left: 90
+    },
     width = 1200 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("#svg-knowledge")
+  // append the svg object to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("#svg-knowledge")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate("
-          + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" +
+      margin.left + "," + margin.top + ")");
 
-var i = 0,
+  var i = 0,
     duration = 750,
     root;
 
-// declares a tree layout and assigns the size
-var treemap = d3.tree().size([height, width]);
+  // declares a tree layout and assigns the size
+  var treemap = d3.tree().size([height, width]);
 
-// Assigns parent, children, height, depth
-root = d3.hierarchy(treeData, function(d) { return d.children; });
-root.x0 = height / 2;
-root.y0 = 0;
+  // Assigns parent, children, height, depth
+  root = d3.hierarchy(treeData, function (d) {
+    return d.children;
+  });
+  root.x0 = height / 2;
+  root.y0 = 0;
 
-// Collapse after the second level
-// root.children.forEach(collapse);
+  // Collapse after the second level
+  // root.children.forEach(collapse);
 
-update(root);
+  update(root);
 
-// Collapse the node and all it's children
-function collapse(d) {
-  if(d.children) {
-    d._children = d.children
-    d._children.forEach(collapse)
-    d.children = null
+  // Collapse the node and all it's children
+  function collapse(d) {
+    if (d.children) {
+      d._children = d.children
+      d._children.forEach(collapse)
+      d.children = null
+    }
   }
-}
 
-function update(source) {
+  function update(source) {
 
-  // Assigns the x and y position for the nodes
-  var treeData = treemap(root);
+    // Assigns the x and y position for the nodes
+    var treeData = treemap(root);
 
-  // Compute the new tree layout.
-  var nodes = treeData.descendants(),
+    // Compute the new tree layout.
+    var nodes = treeData.descendants(),
       links = treeData.descendants().slice(1);
 
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d){ d.y = d.depth * 180});
+    // Normalize for fixed-depth.
+    nodes.forEach(function (d) {
+      d.y = d.depth * 180
+    });
 
-  // ****************** Nodes section ***************************
+    // ****************** Nodes section ***************************
 
-  // Update the nodes...
-  var node = svg.selectAll('g.node')
-      .data(nodes, function(d) {return d.id || (d.id = ++i); });
+    // Update the nodes...
+    var node = svg.selectAll('g.node')
+      .data(nodes, function (d) {
+        return d.id || (d.id = ++i);
+      });
 
-  // Enter any new modes at the parent's previous position.
-  var nodeEnter = node.enter().append('g')
+    // Enter any new modes at the parent's previous position.
+    var nodeEnter = node.enter().append('g')
       .attr('class', 'node')
-      .attr("transform", function(d) {
+      .attr("transform", function (d) {
         return "translate(" + source.y0 + "," + source.x0 + ")";
-    })
-    .on('click', click);
+      })
+      .on('click', click);
 
-  // Add Circle for the nodes
-  nodeEnter.append('circle')
+    // Add Circle for the nodes
+    nodeEnter.append('circle')
       .attr('class', 'node')
       .attr('r', 1e-6)
-      .style("fill", function(d) {
-          return d._children ? "lightsteelblue" : "#fff";
-      });
-
-  // Add labels for the nodes
-  nodeEnter.append('text')
-      .attr("dy", ".35em")
-      .attr("x", function(d) {
-          return d.children || d._children ? -13 : 13;
-      })
-      .attr("text-anchor", function(d) {
-          return d.children || d._children ? "end" : "start";
-      })
-      .text(function(d) { return d.data.name; });
-
-  // UPDATE
-  var nodeUpdate = nodeEnter.merge(node);
-
-  // Transition to the proper position for the node
-  nodeUpdate.transition()
-    .duration(duration)
-    .attr("transform", function(d) { 
-        return "translate(" + d.y + "," + d.x + ")";
-     });
-
-  // Update the node attributes and style
-  nodeUpdate.select('circle.node')
-    .attr('r', 5)
-    .style("fill", function(d) {
+      .style("fill", function (d) {
         return d._children ? "lightsteelblue" : "#fff";
-    })
-    .attr('cursor', 'pointer');
+      });
 
+    // Add labels for the nodes
+    nodeEnter.append('text')
+      .attr("dy", ".35em")
+      .attr("x", function (d) {
+        return d.children || d._children ? -13 : 13;
+      })
+      .attr("text-anchor", function (d) {
+        return d.children || d._children ? "end" : "start";
+      })
+      .text(function (d) {
+        return d.data.name;
+      });
 
-  // Remove any exiting nodes
-  var nodeExit = node.exit().transition()
+    // UPDATE
+    var nodeUpdate = nodeEnter.merge(node);
+
+    // Transition to the proper position for the node
+    nodeUpdate.transition()
       .duration(duration)
-      .attr("transform", function(d) {
-          return "translate(" + source.y + "," + source.x + ")";
+      .attr("transform", function (d) {
+        return "translate(" + d.y + "," + d.x + ")";
+      });
+
+    // Update the node attributes and style
+    nodeUpdate.select('circle.node')
+      .attr('r', 5)
+      .style("fill", function (d) {
+        return d._children ? "lightsteelblue" : "#fff";
+      })
+      .attr('cursor', 'pointer');
+
+
+    // Remove any exiting nodes
+    var nodeExit = node.exit().transition()
+      .duration(duration)
+      .attr("transform", function (d) {
+        return "translate(" + source.y + "," + source.x + ")";
       })
       .remove();
 
-  // On exit reduce the node circles size to 0
-  nodeExit.select('circle')
-    .attr('r', 1e-6);
+    // On exit reduce the node circles size to 0
+    nodeExit.select('circle')
+      .attr('r', 1e-6);
 
-  // On exit reduce the opacity of text labels
-  nodeExit.select('text')
-    .style('fill-opacity', 1e-6);
+    // On exit reduce the opacity of text labels
+    nodeExit.select('text')
+      .style('fill-opacity', 1e-6);
 
-  // ****************** links section ***************************
+    // ****************** links section ***************************
 
-  // Update the links...
-  var link = svg.selectAll('path.link')
-      .data(links, function(d) { return d.id; });
+    // Update the links...
+    var link = svg.selectAll('path.link')
+      .data(links, function (d) {
+        return d.id;
+      });
 
-  // Enter any new links at the parent's previous position.
-  var linkEnter = link.enter().insert('path', "g")
+    // Enter any new links at the parent's previous position.
+    var linkEnter = link.enter().insert('path', "g")
       .attr("class", "link")
-      .attr('d', function(d){
-        var o = {x: source.x0, y: source.y0}
+      .attr('d', function (d) {
+        var o = {
+          x: source.x0,
+          y: source.y0
+        }
         return diagonal(o, o)
       });
 
-  // UPDATE
-  var linkUpdate = linkEnter.merge(link);
+    // UPDATE
+    var linkUpdate = linkEnter.merge(link);
 
-  // Transition back to the parent element position
-  linkUpdate.transition()
+    // Transition back to the parent element position
+    linkUpdate.transition()
       .duration(duration)
-      .attr('d', function(d){ return diagonal(d, d.parent) });
+      .attr('d', function (d) {
+        return diagonal(d, d.parent)
+      });
 
-  // Remove any exiting links
-  var linkExit = link.exit().transition()
+    // Remove any exiting links
+    var linkExit = link.exit().transition()
       .duration(duration)
-      .attr('d', function(d) {
-        var o = {x: source.x, y: source.y}
+      .attr('d', function (d) {
+        var o = {
+          x: source.x,
+          y: source.y
+        }
         return diagonal(o, o)
       })
       .remove();
 
-  // Store the old positions for transition.
-  nodes.forEach(function(d){
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
+    // Store the old positions for transition.
+    nodes.forEach(function (d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
 
-  // Creates a curved (diagonal) path from parent to the child nodes
-  function diagonal(s, d) {
-    var path = 'M ' + s.y + ' ' + s.x + ' C ' + (s.y + d.y) / 2 + ' ' + s.x + ' , ' + (s.y + d.y) / 2 + ' ' + d.x + ' , ' + ' ' + d.y  + ' ' + d.x;
-    return path;
-  }
+    // Creates a curved (diagonal) path from parent to the child nodes
+    function diagonal(s, d) {
+      var path = 'M ' + s.y + ' ' + s.x + ' C ' + (s.y + d.y) / 2 + ' ' + s.x + ' , ' + (s.y + d.y) / 2 + ' ' + d.x + ' , ' + ' ' + d.y + ' ' + d.x;
+      return path;
+    }
 
-  // Toggle children on click.
-  function click(d) {
-    if (d.children) {
+    // Toggle children on click.
+    function click(d) {
+      if (d.children) {
         d._children = d.children;
         d.children = null;
       } else {
         d.children = d._children;
         d._children = null;
       }
-    update(d);
+      update(d);
+    }
   }
-}
 })()
